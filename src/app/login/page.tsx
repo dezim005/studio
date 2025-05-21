d46ad6294447526c836116efad5017941e8eeb67
DEZIMBOLADAO <s.andre2012.AS@gmail.com>
2025-05-21 23:46:04 +0000

    centralizando o login em telas maiores

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -66,7 +66,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background p-4 sm:p-6 md:p-8">
+    <div className="w-full flex min-h-screen items-center justify-center bg-background p-4 sm:p-6 md:p-8">
       <div className="w-full max-w-md space-y-8 flex flex-col items-center">
         <div className="flex justify-center">
           <Logo />
